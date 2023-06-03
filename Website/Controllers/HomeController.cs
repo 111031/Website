56e9166c3c7cb5113c2162f112a41992ab7636c3
Julian Kuijf <juliankuijf@gmail.com>
2023-06-03 10:26:51 +0200

    artikel panda toegevoegd

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -107,6 +107,12 @@ namespace Website.Controllers
             return View();
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("artikelpanda")]
+        public IActionResult Artikelpanda()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
